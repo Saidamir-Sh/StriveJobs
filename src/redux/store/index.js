bbc9378bcd1f070bde9fdbcc16c52ab01e79012f
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-01-26 15:01:17 +0100

    configure Store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,2 +1,15 @@
 import { createStore } from 'redux'
 
+export const initialState = {
+    saved: {
+        jobs: [],
+    },
+}
+
+let configureStore = createStore(
+    mainReducer,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+export default configureStore
